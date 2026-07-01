099a7b23d3315375da85cc1b0d6a8a8943dcc518
nejlamulayim11 <ntysoftware@gmail.com>
2026-07-01 10:35:32 +0300

    Static files uploaded

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+
+const myLogger = (req,res,next) =>{
+    console.log("middleware log 1");
+    next(); 
+}
+
+//Middleware
+app.use(express.static('public'));
+app.use(myLogger);
+app.get('/',(req,res) =>{
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  
+})
+
+const port = 3000;
+app.listen(port,() => {
+    console.log(`Sunucu ${port} portunda başlatıldı..`)
+})
